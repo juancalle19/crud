02fb5a0bc35d2ff302b550d203da8a89001a7a9e
Juanes <jesteban.2741@gmail.com>
2021-02-27 23:46:44 -0500

    button edit in memory

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
 
   const [task, setTask] = useState("")
   const [tasks , setTasks] = useState([])
+  const [editMode, seteditMode] = useState(false)
+  const [id, setId] = useState("")
 
   const addTask = (e) => {
     e.preventDefault()
@@ -21,7 +23,7 @@ function App() {
 
     setTasks([...tasks,newTask])
     setTask("")
-    return
+    
   }
 
   const deleteTask =(id) =>{
@@ -29,6 +31,28 @@ function App() {
     const filteredTasks = tasks.filter(task => task.id !== id)
     setTasks(filteredTasks)
 
+  }
+
+  const editTask =(theTask) =>{
+    setTask(theTask.name)
+    seteditMode(true)
+    setId(theTask.id)
+    console.log(theTask)
+  }
+
+  const saveTask = (e) => {
+    e.preventDefault()
+    if(isEmpty(task)){
+      console.log("Empty")
+      return
+    }
+    
+    const editedTasks = tasks.map(item => item.id === id ? {id, name : task} : item)
+    setTasks(editedTasks)
+    setTask("")
+    seteditMode(false)
+    setId("")
+    
   }
 
   return (
@@ -53,7 +77,8 @@ function App() {
                       <span className = "lead">{tasks.name}</span>
                       <button className = "btn btn-danger btn-sm float-right"
                       onClick = {()=> deleteTask(tasks.id)}>Eliminar</button>
-                      <button className = "btn btn-warning btn-sm float-right mx-2">Editar</button>
+                      <button className = "btn btn-warning btn-sm float-right mx-2"
+                      onClick = {() => editTask(tasks)}>Editar</button>
                     </li>
                   ))
               
@@ -64,16 +89,16 @@ function App() {
           }
         </div>
         <div className = "col-4">
-          <h4 className = "text-center">Formulario</h4>
+          <h4 className = "text-center">{ editMode ? "Modificar Tarea" : "Agregar Tarea"}</h4>
           <hr/>
-          <form onSubmit = {addTask}>
+          <form onSubmit = {editMode ? saveTask : addTask}>
             <input className = "form-control mb-2"
             type= "text"
             placeholder = "Ingrese tarea"
             onChange = {(text) =>{setTask(text.target.value)}}
             value = {task}></input>
-            <button className = "btn btn-dark btn-block"
-            type = "submit">Agregar</button>
+            <button className = {editMode ? "btn btn-warning btn-block" : "btn btn-dark btn-block"}
+            type = "submit">{ editMode ? "Guardar": "Agregar"}</button>
           </form>
         </div>
 
